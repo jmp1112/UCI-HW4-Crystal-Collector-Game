c759f5c8d1b188dc3b7eaa439fb36b97651dee37
jmp1112 <jprickett92@gmail.com>
2017-10-15 23:27:11 -0700

    Add files via upload

---
+++ week4assignment/week-4-game/assets/javascript/game.js
@@ -0,0 +1,5 @@
+// 1) Random number generated for user to guess. Generated at the start of game.
+// 2) 4 randomly generated crystal values (range from 1-12)
+// 3) Wins and loses tracker (w/ an alert that say You Won or You Lost at end of game)
+// 4) Clickable crystals
+// 5) User's score that tracks throughout game (additional guesses are added on)
